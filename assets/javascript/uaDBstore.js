2c8f4caf907e4dd72d45d6044465e7f26120cd44
John Barson <John@NewAdventure.(none)>
2017-06-22 20:42:09 -0400

    UA on multiple pages test

--- assets/javascript/uaDBstore.js
+++ assets/javascript/uaDBstore.js
@@ -1,19 +1,15 @@
-//User Authentication and database storage
-//$(document).ready(function() {
-  // Initialize Firebase
-	var config = {
-		apiKey: "AIzaSyDi2g588bcWLXFwdCjviNcxOLMGQapxjbU",
-		authDomain: "food-paradise-8ef13.firebaseapp.com",
-		databaseURL: "https://food-paradise-8ef13.firebaseio.com",
-		projectId: "food-paradise-8ef13",
-		storageBucket: "food-paradise-8ef13.appspot.com",
-		messagingSenderId: "698241980106"
-	};
-	firebase.initializeApp(config);
-
-	// FirebaseUI config.
+    var config = {
+      apiKey: "AIzaSyDi2g588bcWLXFwdCjviNcxOLMGQapxjbU",
+      authDomain: "food-paradise-8ef13.firebaseapp.com",
+      databaseURL: "https://food-paradise-8ef13.firebaseio.com",
+      projectId: "food-paradise-8ef13",
+      storageBucket: "food-paradise-8ef13.appspot.com",
+      messagingSenderId: "698241980106"
+    };
+    firebase.initializeApp(config);
+      // FirebaseUI config.
       var uiConfig = {
-        signInSuccessUrl: 'https://davesrose.github.io/Food_Paradise/',
+        signInSuccessUrl: '//davesrose.github.io/Food_Paradise/',
         signInOptions: [
           // Leave the lines as is for the providers you want to offer your users.
           firebase.auth.GoogleAuthProvider.PROVIDER_ID,
@@ -27,4 +23,42 @@
       var ui = new firebaseui.auth.AuthUI(firebase.auth());
       // The start method will wait until the DOM is loaded.
       ui.start('#firebaseui-auth-container', uiConfig);
-//});
+      initApp = function() {
+        firebase.auth().onAuthStateChanged(function(user) {
+          if (user) {
+            // User is signed in.
+            var displayName = user.displayName;
+            var email = user.email;
+            var emailVerified = user.emailVerified;
+            var photoURL = user.photoURL;
+            var uid = user.uid;
+            var phoneNumber = user.phoneNumber;
+            var providerData = user.providerData;
+            user.getToken().then(function(accessToken) {
+              document.getElementById('sign-in-status').textContent = 'Signed in';
+              document.getElementById('sign-in').textContent = 'Sign out';
+              document.getElementById('account-details').textContent = JSON.stringify({
+                displayName: displayName,
+                email: email,
+                emailVerified: emailVerified,
+                phoneNumber: phoneNumber,
+                photoURL: photoURL,
+                uid: uid,
+                accessToken: accessToken,
+                providerData: providerData
+              }, null, '  ');
+            });
+          } else {
+            // User is signed out.
+            document.getElementById('sign-in-status').textContent = 'Signed out';
+            document.getElementById('sign-in').textContent = 'Sign in';
+            document.getElementById('account-details').textContent = 'null';
+          }
+        }, function(error) {
+          console.log(error);
+        });
+      };
+
+      window.addEventListener('load', function() {
+        initApp()
+      });
