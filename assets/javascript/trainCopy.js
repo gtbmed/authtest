e6c53ddc706b61a05fd580f9be81b480b0760feb
John Barson <John@NewAdventure.(none)>
2017-06-24 11:05:29 -0400

    remove document ready from trainCopy.js

--- assets/javascript/trainCopy.js
+++ assets/javascript/trainCopy.js
@@ -1,15 +1,14 @@
-$(document).ready(function() {
     // Initialize Firebase
 
-  var config = {
-    apiKey: "AIzaSyB02ri4PZSuQmOv_Z9IDn0HTLs4xsLPY6Q",
-    authDomain: "authentication-testing-231da.firebaseapp.com",
-    databaseURL: "https://authentication-testing-231da.firebaseio.com",
-    projectId: "authentication-testing-231da",
-    storageBucket: "authentication-testing-231da.appspot.com",
-    messagingSenderId: "686766866707"
-  };
-  firebase.initializeApp(config);
+    var config = {
+        apiKey: "AIzaSyB02ri4PZSuQmOv_Z9IDn0HTLs4xsLPY6Q",
+        authDomain: "authentication-testing-231da.firebaseapp.com",
+        databaseURL: "https://authentication-testing-231da.firebaseio.com",
+        projectId: "authentication-testing-231da",
+        storageBucket: "authentication-testing-231da.appspot.com",
+        messagingSenderId: "686766866707"
+    };
+    firebase.initializeApp(config);
 
     var database = firebase.database();
 
@@ -66,77 +65,76 @@ $(document).ready(function() {
         $("#train-frequency").val("");
     });
 
-database.ref().on("child_added", function(childSnapshot, prevChildKey) {
-	console.log(childSnapshot.val());
+    database.ref().on("child_added", function(childSnapshot, prevChildKey) {
+        console.log(childSnapshot.val());
 
-	var recall_name = childSnapshot.val().name;
-	var recall_destination = childSnapshot.val().destination;
-	var recall_startTime = childSnapshot.val().startTime;
-	var recall_frequency = childSnapshot.val().frequency;
+        var recall_name = childSnapshot.val().name;
+        var recall_destination = childSnapshot.val().destination;
+        var recall_startTime = childSnapshot.val().startTime;
+        var recall_frequency = childSnapshot.val().frequency;
 
-	// Calculate Time of Next Train Arrival and minute until next train arrives
-	// convert the Start time of the train to be used by momentJS as HHmm
-	var firstTimeConverted = moment(recall_startTime, "HHmm").subtract(1, "years");
-	// Find the difference in minutes between the start time and the current time
-	var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
-	// Divide the difference by the frequency as the remainder is used in the next calculation
-	var tRemainder = diffTime % recall_frequency;
-	//The frequency each train arrives minus the remainder equals the amount of time till the next train
-	var minutesAway = recall_frequency - tRemainder;
-	// Add the minutes from arrival to the current time to give an arrival time
-	var nextTrain = moment().add(minutesAway, "minutes");
-	//Store the arrival time in a usable format
-	var nextArrival = moment(nextTrain, "HHmm").format("h:mm A");
-	// add an entry to the train table
-	$("#trainTable > tbody").append("<tr><td>" + recall_name + "</td><td>" + recall_destination + "</td><td>" + recall_frequency + "</td><td>" + nextArrival + "</td><td>" + minutesAway  + "</td></tr>");
+        // Calculate Time of Next Train Arrival and minute until next train arrives
+        // convert the Start time of the train to be used by momentJS as HHmm
+        var firstTimeConverted = moment(recall_startTime, "HHmm").subtract(1, "years");
+        // Find the difference in minutes between the start time and the current time
+        var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+        // Divide the difference by the frequency as the remainder is used in the next calculation
+        var tRemainder = diffTime % recall_frequency;
+        //The frequency each train arrives minus the remainder equals the amount of time till the next train
+        var minutesAway = recall_frequency - tRemainder;
+        // Add the minutes from arrival to the current time to give an arrival time
+        var nextTrain = moment().add(minutesAway, "minutes");
+        //Store the arrival time in a usable format
+        var nextArrival = moment(nextTrain, "HHmm").format("h:mm A");
+        // add an entry to the train table
+        $("#trainTable > tbody").append("<tr><td>" + recall_name + "</td><td>" + recall_destination + "</td><td>" + recall_frequency + "</td><td>" + nextArrival + "</td><td>" + minutesAway + "</td></tr>");
 
-	} , function(errorObject) {
-		console.log("Errors handled: " + errorObject.code);
+    }, function(errorObject) {
+        console.log("Errors handled: " + errorObject.code);
 
-});
+    });
 
 
-      // Initialize the FirebaseUI Widget using Firebase.
-      var ui = new firebaseui.auth.AuthUI(firebase.auth());
-      // The start method will wait until the DOM is loaded.
-      ui.start('#firebaseui-auth-container', uiConfig);
-      initApp = function() {
+    // Initialize the FirebaseUI Widget using Firebase.
+    var ui = new firebaseui.auth.AuthUI(firebase.auth());
+    // The start method will wait until the DOM is loaded.
+    ui.start('#firebaseui-auth-container', uiConfig);
+    initApp = function() {
         firebase.auth().onAuthStateChanged(function(user) {
-          if (user) {
-            // User is signed in.
-            var displayName = user.displayName;
-            var email = user.email;
-            var emailVerified = user.emailVerified;
-            var photoURL = user.photoURL;
-            var uid = user.uid;
-            var phoneNumber = user.phoneNumber;
-            var providerData = user.providerData;
-            user.getIdToken().then(function(accessToken) {
-              document.getElementById('sign-in-status').textContent = 'Signed in';
-              document.getElementById('sign-in').textContent = 'Sign out';
-              document.getElementById('account-details').textContent = JSON.stringify({
-                displayName: displayName,
-                email: email,
-                emailVerified: emailVerified,
-                phoneNumber: phoneNumber,
-                photoURL: photoURL,
-                uid: uid,
-                accessToken: accessToken,
-                providerData: providerData
-              }, null, '  ');
-            });
-          } else {
-            // User is signed out.
-            document.getElementById('sign-in-status').textContent = 'Signed out';
-            document.getElementById('sign-in').textContent = 'Sign in';
-            document.getElementById('account-details').textContent = 'null';
-          }
+            if (user) {
+                // User is signed in.
+                var displayName = user.displayName;
+                var email = user.email;
+                var emailVerified = user.emailVerified;
+                var photoURL = user.photoURL;
+                var uid = user.uid;
+                var phoneNumber = user.phoneNumber;
+                var providerData = user.providerData;
+                user.getIdToken().then(function(accessToken) {
+                    document.getElementById('sign-in-status').textContent = 'Signed in';
+                    document.getElementById('sign-in').textContent = 'Sign out';
+                    document.getElementById('account-details').textContent = JSON.stringify({
+                        displayName: displayName,
+                        email: email,
+                        emailVerified: emailVerified,
+                        phoneNumber: phoneNumber,
+                        photoURL: photoURL,
+                        uid: uid,
+                        accessToken: accessToken,
+                        providerData: providerData
+                    }, null, '  ');
+                });
+            } else {
+                // User is signed out.
+                document.getElementById('sign-in-status').textContent = 'Signed out';
+                document.getElementById('sign-in').textContent = 'Sign in';
+                document.getElementById('account-details').textContent = 'null';
+            }
         }, function(error) {
-          console.log(error);
+            console.log(error);
         });
-      };
+    };
 
-      window.addEventListener('load', function() {
+    window.addEventListener('load', function() {
         initApp()
-      });
-});
+    });
