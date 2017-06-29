4333a46c177a77cf732e613fc3e635bacdc70bfc
gtbmed <jkbarson@gmail.com>
2017-06-28 22:28:04 -0400

    Change strategy, no more Auth UI, just use email auth

--- assets/javascript/uaDB.js
+++ assets/javascript/uaDB.js
@@ -7,29 +7,52 @@
     messagingSenderId: "686766866707"
   };
   firebase.initializeApp(config);
-      // FirebaseUI config.
-      var uiConfig = {
-        callbacks: {
-          signInSuccess: function(currentUser, credential, redirectUrl) {
-            // Do something.
-            // Return type determines whether we continue the redirect automatically
-            // or whether we leave that to developer to handle.
-            return true;
-          }
-        }
-        signInSuccessUrl: '//gtbmed.github.io/authtest/page2.html',
-        signInOptions: [
-          // Leave the lines as is for the providers you want to offer your users.
-          firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-          firebase.auth.EmailAuthProvider.PROVIDER_ID
-        ],
-        // Terms of service url.
-        tosUrl: '<your-tos-url>'
-      };
-      // Initialize the FirebaseUI Widget using Firebase.
-      var ui = new firebaseui.auth.AuthUI(firebase.auth());
-      // The start method will wait until the DOM is loaded.
-      ui.start('#firebaseui-auth-container', uiConfig);
+
+var user = firebase.auth().currentUser;
+auth.signInWithEmailAndPassword(email, pass);
+auth.createUserWithEmailAndPassword(email, pass);
+
+
+var database = firebase.database();
+function storeRecipe()
+firebase.auth.onAuthStateChanged(firebaseUser => {
+
+if (firebaseUser != null) {
+
+}
+
+// if (user != null) {
+//   user.providerData.forEach(function (profile) {
+//     console.log("Sign-in provider: "+profile.providerId);
+//     console.log("  Provider-specific UID: "+profile.uid);
+//     console.log("  Name: "+profile.displayName);
+//     console.log("  Email: "+profile.email);
+//     console.log("  Photo URL: "+profile.photoURL);
+//   });
+// }
+      // // FirebaseUI config.
+      // var uiConfig = {
+      //   callbacks: {
+      //     signInSuccess: function(currentUser, credential, redirectUrl) {
+      //       // Do something.
+      //       // Return type determines whether we continue the redirect automatically
+      //       // or whether we leave that to developer to handle.
+      //       return true;
+      //     }
+      //   }
+      //   signInSuccessUrl: '//gtbmed.github.io/authtest/page2.html',
+      //   signInOptions: [
+      //     // Leave the lines as is for the providers you want to offer your users.
+      //     firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+      //     firebase.auth.EmailAuthProvider.PROVIDER_ID
+      //   ],
+      //   // Terms of service url.
+      //   tosUrl: '<your-tos-url>'
+      // };
+      // // Initialize the FirebaseUI Widget using Firebase.
+      // var ui = new firebaseui.auth.AuthUI(firebase.auth());
+      // // The start method will wait until the DOM is loaded.
+      // ui.start('#firebaseui-auth-container', uiConfig);
 
 
   // firebase.auth.onAuthStateChanged(firebaseUser => {
@@ -39,20 +62,7 @@
   //     btnLogout.classList.add('hide');
   //   }
   // });
-var user = firebase.auth().currentUser;
 
-if (user != null) {
-  user.providerData.forEach(function (profile) {
-    console.log("Sign-in provider: "+profile.providerId);
-    console.log("  Provider-specific UID: "+profile.uid);
-    console.log("  Name: "+profile.displayName);
-    console.log("  Email: "+profile.email);
-    console.log("  Photo URL: "+profile.photoURL);
-  });
-}
-
-
-// var database = firebase.database();
 
 //         ref.child("users").child(authData.uid).set({
 //           provider: authData.provider,
